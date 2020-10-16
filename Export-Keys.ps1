5022a1f7495a04e74a943447258f651045b7e735
Beginin Vladimir <beginin@mail.ru>
2020-10-16 23:14:16 +0700

    Init commit

---
+++ Export-Keys.ps1
@@ -0,0 +1,40 @@
+﻿$savedir=$env:APPDATA + "\Keys\"
+$remotedir = "C:\1\"
+$CryptoPath = "HKLM:\SOFTWARE\Wow6432Node\Crypto Pro\Settings\Users\"
+$sid = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value
+$UserPath = $CryptoPath + $sid + "\Keys"
+
+foreach ($key in Get-Item -Path $UserPath  | Get-ChildItem -ErrorAction SilentlyContinue -Recurse) {
+   
+    try {
+ 
+        $folder = $savedir + $key.Name.Split("\\")[-1]
+        $remotefolder = $remotedir + $key.Name.Split("\\")[-1]
+        $header = Get-ItemProperty -Path $key.PSPath -Name header.key -ErrorAction Stop
+        $masks = Get-ItemProperty -Path $key.PSPath -Name masks.key -ErrorAction Stop
+        $masks2 = Get-ItemProperty -Path $key.PSPath -Name masks2.key -ErrorAction Stop
+        $name = Get-ItemProperty -Path $key.PSPath -Name name.key -ErrorAction Stop
+        $primary = Get-ItemProperty -Path $key.PSPath -Name primary.key -ErrorAction Stop
+        $primary2 = Get-ItemProperty -Path $key.PSPath -Name primary2.key -ErrorAction Stop
+        New-Item -ItemType Directory -Force -Path $folder
+        [io.file]::WriteAllBytes($folder + '\header.key',$header.'header.key')
+        [io.file]::WriteAllBytes($folder + '\masks.key',$masks.'masks.key')
+        [io.file]::WriteAllBytes($folder + '\masks2.key',$masks2.'masks2.key')
+        [io.file]::WriteAllBytes($folder + '\name.key',$name.'name.key')
+        [io.file]::WriteAllBytes($folder + '\primary.key',$primary.'primary.key')
+        [io.file]::WriteAllBytes($folder + '\primary2.key',$primary2.'primary2.key')
+        New-Item -ItemType Directory -Force -Path $remotefolder
+        [io.file]::WriteAllBytes($remotefolder + '\header.key',$header.'header.key')
+        [io.file]::WriteAllBytes($remotefolder + '\masks.key',$masks.'masks.key')
+        [io.file]::WriteAllBytes($remotefolder + '\masks2.key',$masks2.'masks2.key')
+        [io.file]::WriteAllBytes($remotefolder + '\name.key',$name.'name.key')
+        [io.file]::WriteAllBytes($remotefolder + '\primary.key',$primary.'primary.key')
+        [io.file]::WriteAllBytes($remotefolder + '\primary2.key',$primary2.'primary2.key')
+
+    }
+    catch {
+        continue
+    }
+    
+}
+
---
+++ Import-Keys.ps1
@@ -0,0 +1,41 @@
+﻿$keyDir=$env:APPDATA + "\Keys\"
+$CryptoPath = "HKLM:\SOFTWARE\Wow6432Node\Crypto Pro\Settings\Users\"
+$sid = [System.Security.Principal.WindowsIdentity]::GetCurrent().user.value
+$UserPath = $CryptoPath + $sid + "\Keys"
+
+foreach ($key in (Get-Item -Path $keyDir |  Get-ChildItem -ErrorAction SilentlyContinue)) {
+    Write-Host "----"
+    $folder = $key.PSPath.Split(":")[-2] + ":" + $key.PSPath.Split(":")[-1]
+    try {
+        #$bytes = [System.IO.File]::ReadAllBytes()
+        $header = [io.file]::ReadAllBytes($folder + '\header.key')
+        $masks = [io.file]::ReadAllBytes($folder + '\masks.key')
+        $masks2 = [io.file]::ReadAllBytes($folder + '\masks2.key')
+        $name = [io.file]::ReadAllBytes($folder + '\name.key')
+        $primary = [io.file]::ReadAllBytes($folder + '\primary.key')
+        $primary2 = [io.file]::ReadAllBytes($folder + '\primary2.key')
+        $keyName = [System.Text.Encoding]::GetEncoding(1251).GetString($name[4..($name.length)])
+        if ((Test-Path -Path ($CryptoPath + $sid)) -eq $false) {
+            New-Item -Path $CryptoPath -Name $sid
+        }
+        if ((Test-Path -Path $UserPath) -eq $false) {
+            New-Item -Path ($CryptoPath + $sid) -Name "Keys"
+        }
+        if ((Test-Path -Path ($UserPath +"\"+$keyName )) -eq $false) {
+            New-Item -Path $UserPath -Name $keyName
+            New-ItemProperty -Path ($UserPath +"\"+$keyName ) -Name 'header.key' -Value $header
+            New-ItemProperty -Path ($UserPath +"\"+$keyName ) -Name 'masks.key' -Value $masks
+            New-ItemProperty -Path ($UserPath +"\"+$keyName ) -Name 'masks2.key' -Value $masks2
+            New-ItemProperty -Path ($UserPath +"\"+$keyName ) -Name 'name.key' -Value $name
+            New-ItemProperty -Path ($UserPath +"\"+$keyName ) -Name 'primary.key' -Value $primary
+            New-ItemProperty -Path ($UserPath +"\"+$keyName ) -Name 'primary2.key' -Value $primary2
+
+        }       
+
+    }
+    catch {
+        continue
+    }
+
+}
+
